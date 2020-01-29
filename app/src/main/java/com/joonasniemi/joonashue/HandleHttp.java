cdb462d9955f07e58d9611d82925460905df2d0f
jopakka <jopakkamies@gmail.com>
2020-01-29 17:44:08 +0200

    gets own username

--- app/src/main/java/com/joonasniemi/joonashue/HandleHttp.java
+++ app/src/main/java/com/joonasniemi/joonashue/HandleHttp.java
@@ -13,10 +13,12 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -24,8 +26,24 @@ import java.net.URLConnection;
 
 import javax.net.ssl.HttpsURLConnection;
 
-public class ReadUrl extends AsyncTask<String, Void, String> {
+public class HandleHttp extends AsyncTask<String, Void, String> {
     private static String TAG = "myTag";
+    private String method;
+    private JSONObject jObj;
+
+    public HandleHttp(){
+        this.method = "GET";
+    }
+
+    public HandleHttp(String method){
+        this.method = method;
+    }
+
+    public HandleHttp(String method, JSONObject jObj){
+        this.method = method;
+        this.jObj = jObj;
+    }
+
 
     @Override
     protected String doInBackground(String... urls) {
@@ -34,19 +52,30 @@ public class ReadUrl extends AsyncTask<String, Void, String> {
         try {
             url = new URL(urls[0]);
             conn = (HttpURLConnection) url.openConnection();
+            if(method.equals("POST")){
+                conn.setDoOutput(true);
+                OutputStream out = new BufferedOutputStream(conn.getOutputStream());
+                out.write(jObj.toJSONString().getBytes());
+                out.flush();
+                Log.d(TAG, "Out: " + out);
+            } else {
+                conn.setRequestMethod(method);
+            }
             BufferedReader buff = new BufferedReader(new InputStreamReader(conn.getInputStream()));
             String line = buff.readLine();
             Log.d(TAG, "InputStream: " + line);
             return line;
 
-        } catch (MalformedURLException e){
+        } catch (MalformedURLException e) {
             Log.e(TAG, "Incorrect url: " + e);
-        } catch (IOException e){
+        } catch (IOException e) {
             Log.e(TAG, "Failed to connect: " + e);
-        } catch (Exception e){
+        } catch (Exception e) {
             Log.e(TAG, "Random error: " + e);
         } finally {
-            conn.disconnect();
+            if(conn != null){
+                conn.disconnect();
+            }
         }
         return null;
     }
---
+++ app/src/main/java/com/joonasniemi/joonashue/SetupActivity.java
@@ -0,0 +1,116 @@
+package com.joonasniemi.joonashue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.util.concurrent.ExecutionException;
+
+public class SetupActivity extends AppCompatActivity implements View.OnClickListener {
+    private static String TAG = "myTag";
+    private static String upnp = "https://discovery.meethue.com/";
+    private String bridgeIp;
+    private String username;
+    private TextView tvIp;
+    private TextView tvUser;
+
+    private SharedPreferences pref;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setup);
+        pref = getSharedPreferences("hueInfo", Context.MODE_PRIVATE);
+        bridgeIp = pref.getString("bridgeIp", null);
+        username = pref.getString("username", null);
+
+        findViewById(R.id.bBridgeIp).setOnClickListener(this);
+        findViewById(R.id.bDev).setOnClickListener(this);
+        findViewById(R.id.bLogin).setOnClickListener(this);
+
+        tvIp = findViewById(R.id.tvIp);
+        tvUser = findViewById(R.id.tvUser);
+        tvIp.setText(bridgeIp);
+        tvUser.setText(username);
+
+
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putString("username", username);
+        editor.putString("bridgeIp", bridgeIp);
+        editor.apply();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.bBridgeIp) {
+            Log.d(TAG, "bBridgeIp pressed");
+            findBridgeIp();
+            tvIp.setText(bridgeIp);
+        } else if (v.getId() == R.id.bDev){
+            Log.d(TAG, "bDev pressed");
+            setDevToBridge();
+            tvUser.setText(username);
+        } else if (v.getId() == R.id.bLogin){
+            Log.d(TAG, "bLogin pressed");
+            setDevToBridge();
+        }
+    }
+
+    private void findBridgeIp() {
+        try {
+            String line = new HandleHttp().execute(upnp).get();
+            Log.d(TAG, "Line: " + line);
+            JSONParser parser = new JSONParser();
+            JSONArray jArray = (JSONArray) parser.parse(line);
+            JSONObject jObj = (JSONObject) jArray.get(0);
+            bridgeIp = (String) jObj.get("internalipaddress");
+            Log.d(TAG, "IP-Address: " + bridgeIp);
+        } catch (ParseException e) {
+            Log.e(TAG, "ParseException: " + e);
+        } catch (ExecutionException e) {
+            Log.e(TAG, "ExecutionException: " + e);
+        } catch (InterruptedException e) {
+            Log.e(TAG, "InterruptedException: " + e);
+        }
+    }
+
+    private void setDevToBridge() {
+        try {
+            JSONObject dev = new JSONObject();
+            dev.put("devicetype", R.string.app_name + "#Admin");
+            String line = new HandleHttp("POST", dev).execute("http://" + bridgeIp + "/api").get();
+            Log.d(TAG, "Line: " + line);
+            JSONParser parser = new JSONParser();
+            JSONArray jArray = (JSONArray) parser.parse(line);
+            JSONObject jObj = (JSONObject) jArray.get(0);
+            JSONObject success = (JSONObject) jObj.get("success");
+            username = (String) success.get("username");
+            Log.d(TAG, "Dev username: " + username);
+        } catch (ExecutionException e) {
+            Log.e(TAG, "ExecutionException: " + e);
+        } catch (InterruptedException e) {
+            Log.e(TAG, "InterruptedException: " + e);
+        } catch (Exception e){
+            Log.e(TAG, "Random error: " + e);
+        }
+    }
+
+    private void login(){
+        
+    }
+}
